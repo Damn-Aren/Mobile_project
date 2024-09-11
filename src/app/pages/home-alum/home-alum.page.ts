52c623cf17561eb6f7d6caff7d7ae83315d2b2b1
Iriata <victoria.bahamondes.118@gmail.com>
2024-09-11 09:25:01 -0300

    home-alumno

--- src/app/pages/home-alum/home-alum.page.ts
+++ src/app/pages/home-alum/home-alum.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-alum',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeAlumPage implements OnInit {
 
-  constructor() { }
+  usuario:string=''
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit(): void {
+    var x = localStorage.getItem('usuario')
+    this.usuario=x ??''
   }
-
 }
