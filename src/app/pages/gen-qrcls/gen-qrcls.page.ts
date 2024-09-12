0a5dfeb2418d27ac981bc738e93b014d1962bc20
Iriata <victoria.bahamondes.118@gmail.com>
2024-09-12 15:10:51 -0300

    casi todas creo

--- src/app/pages/gen-qrcls/gen-qrcls.page.ts
+++ src/app/pages/gen-qrcls/gen-qrcls.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-gen-qrcls',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenQRClsPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl:NavController) {}
+
 
   ngOnInit() {
+  }
+
+  LeerQR(){
+    this.navCtrl.navigateForward(['generar-qr'])
   }
 
 }
