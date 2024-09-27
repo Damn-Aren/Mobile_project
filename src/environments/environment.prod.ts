af8c1638fc510e20596eb3c252f392185a6799b2
Damn-Aren <benjitusconcha0@gmail.com>
2024-09-27 08:50:16 -0300

    Firestore wia

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDJBKlTyILq9PVOupbCKq7FY2j7VdUm-9I",
+    authDomain: "mobile-project008.firebaseapp.com",
+    projectId: "mobile-project008",
+    storageBucket: "mobile-project008.appspot.com",
+    messagingSenderId: "540822964188",
+    appId: "1:540822964188:web:8d6c2c1add61ee74f73119"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDJBKlTyILq9PVOupbCKq7FY2j7VdUm-9I",
+    authDomain: "mobile-project008.firebaseapp.com",
+    projectId: "mobile-project008",
+    storageBucket: "mobile-project008.appspot.com",
+    messagingSenderId: "540822964188",
+    appId: "1:540822964188:web:8d6c2c1add61ee74f73119"
+  },
 };
 
 /*
