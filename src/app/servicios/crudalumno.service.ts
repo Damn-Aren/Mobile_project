e24abf28b8d72fab673c969ff6f4ad2504334bcb
Iriata <victoria.bahamondes.118@gmail.com>
2024-09-30 18:19:33 -0300

    AAAAAAAAAAAA

--- src/app/servicios/crudalumno.service.ts
+++ src/app/servicios/crudalumno.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
+import { Alumno } from '../model/Alumnos';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class CrudalumnoService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  grabar(alumno:Alumno){
+    return this.afs.collection('alumno').add(alumno);
+  }
 }
