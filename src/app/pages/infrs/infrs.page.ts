9fcc07116fa87643dfe83b3fdfd90aaddc33acd8
Iriata <victoria.bahamondes.118@gmail.com>
2024-10-29 21:46:05 -0300

    PDF parte 1

--- src/app/pages/infrs/infrs.page.ts
+++ src/app/pages/infrs/infrs.page.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import {NavController} from '@ionic/angular';
 
+import pdfMake from "pdfmake/build/pdfmake"; 
+import pdfFonts from "pdfmake/build/vfs_fonts";
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+
 @Component({
   selector: 'app-infrs',
   templateUrl: './infrs.page.html',
   styleUrls: ['./infrs.page.scss'],
 })
 export class InfrsPage implements OnInit {
+
+  ObjectPDF : any; 
 
   constructor(private navCtrl:NavController) {}
 
@@ -17,4 +25,14 @@ export class InfrsPage implements OnInit {
     this.navCtrl.navigateRoot(['/home']);
   }
   
+  descargarPDF(){
+    var dd = {
+      content: [
+        'First paragraph',
+        'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+      ]
+      
+    }
+    this.ObjectPDF = pdfMake.createPdf(dd);
+  }
 }
