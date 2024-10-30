7a7dae46d8002081e45ee4cf6e581a1ad8db5b8e
Damn-Aren <benjitusconcha0@gmail.com>
2024-10-29 22:29:46 -0300

    lista ordenada

--- src/app/servicios/crudasignatura.service.ts
+++ src/app/servicios/crudasignatura.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore'; 
 import { Observable } from 'rxjs';
-import { asignatura01 } from '../model/asignatura'; // Aquí la importación correcta
+import { asignatura01 } from '../model/asignatura';
 
 @Injectable({
   providedIn: 'root'
